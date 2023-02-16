8ffce8c5be6df06d08176ea7baea30b50437628d
efrunevdi <efruncetkin@gmail.com>
2023-02-16 17:38:42 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // Kod içinde bir değerin protected olarak tanımlanması; o değere, bulunduğu class ve ondan türetilen diğer sınıflar içinden erişilebilir olduğunu göstermektedir.
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLOCALDB;database=TraversalDB;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> Abouts2 { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Features2 { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+            
+    }
+}
---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        [Key]
+        public int FeatureID { get; set; }
+        public string Post1Title { get; set; }
+        public string Post1Description { get; set; }
+        public string Post1Image { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
