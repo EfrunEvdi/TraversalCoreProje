d9b3cf1ce1ff9fc35f3617dc2991592e092cf229
efrunevdi <efruncetkin@gmail.com>
2023-02-27 21:20:12 +0300

    50. Ders Sonu

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Destination", "Admin");
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Destination", "Admin");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Destination", "Admin");
+        }
+    }
+}
--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -86,6 +86,14 @@ namespace TraversalCoreProje
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
