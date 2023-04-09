8b1907e23096e9558913a1c0b18be1c3467e8ca6
efrunevdi <efruncetkin@gmail.com>
2023-04-09 16:32:42 +0300

    95. Ders Sonu

---
+++ TraversalCoreProje/Areas/Member/Controllers/LastDestinations.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Abstarct;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class LastDestinations : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public LastDestinations(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _destinationService.TGetLast4Destinations();
+            return View(values);
+        }
+    }
+}
